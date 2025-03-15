fdb3c08cf1e829f8cbbcc52ff1d0690146375f11
teihenn <hm.hr.yossy@gmail.com>
2025-03-16 00:27:14 +0900

    Add which-key.lua

---
+++ .config/nvim/lua/yyossy/plugins/which-key.lua
@@ -0,0 +1,15 @@
+-- Which-key is great for seeing what keymaps you can use.
+
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 1000
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
