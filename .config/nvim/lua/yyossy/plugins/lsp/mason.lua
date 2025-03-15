a66e73b1f1e4c2db88112802573c556b8ac53d1d
teihenn <hm.hr.yossy@gmail.com>
2025-03-16 02:13:43 +0900

    feat(nvim): Add code formatting support with conform.nvim
    - Add new formatting.lua plugin configuration
    - Configure formatters for multiple languages (JavaScript, TypeScript, CSS, HTML, JSON, YAML, Markdown, Lua, Python)
    - Set up format on save with LSP fallback
    - Add keymapping for manual formatting (<leader>mp)
    - Integrate mason-tool-installer for formatter tools
    - Ensure prettier, stylua, and ruff formatters are installed

--- .config/nvim/lua/yyossy/plugins/lsp/mason.lua
+++ .config/nvim/lua/yyossy/plugins/lsp/mason.lua
@@ -1,52 +1,63 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-      ensure_installed = {
-        "ansiblels",
-        "bashls",
-        "clangd",
-        "cmake",
-        "docker_compose_language_service",
-        "dockerls",
-        "eslint", -- javascript/typescript
-        "gopls",
-        -- "groovyls",
-        "harper_ls", -- toml
-        "html",
-        "jdtls", -- java
-        "jinja_lsp",
-        "jsonls",
-        "lua_ls",
-        "ltex", -- reStructuredText
-        "nginx_language_server",
-        "pyright", -- python
-        "rust_analyzer", -- python
-        "sqls",
-        "tflint", -- terraform
-              },
-    })
-  end,
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			-- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+			ensure_installed = {
+				"ansiblels",
+				"bashls",
+				"clangd",
+				"cmake",
+				"docker_compose_language_service",
+				"dockerls",
+				"eslint", -- javascript/typescript
+				"gopls",
+				-- "groovyls",
+				"harper_ls", -- toml
+				"html",
+				"jdtls", -- java
+				"jinja_lsp",
+				"jsonls",
+				"lua_ls",
+				"ltex", -- reStructuredText
+				"nginx_language_server",
+				"pyright", -- python
+				"rust_analyzer", -- python
+				"sqls",
+				"tflint", -- terraform
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"ruff", -- python formatter/linter
+			},
+		})
+	end,
 }
