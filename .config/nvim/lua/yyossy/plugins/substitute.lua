e42d21830fd7589f4c30537fe0f532d3610f5a51
teihenn <hm.hr.yossy@gmail.com>
2025-03-16 00:31:09 +0900

    Add substitute.lua

---
+++ .config/nvim/lua/yyossy/plugins/substitute.lua
@@ -0,0 +1,19 @@
+-- This plugin allows us to use s followed by a motion to substitute text that was previously copied.
+
+return {
+  "gbprod/substitute.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local substitute = require("substitute")
+
+    substitute.setup()
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+    vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+    vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+  end,
+}
