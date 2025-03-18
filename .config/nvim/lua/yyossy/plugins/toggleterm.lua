4c0fdc71b3a5619392baf9d445923bf247c07709
yyossy <39612448+teihenn@users.noreply.github.com>
2025-03-18 15:54:46 +0900

    Update toggleterm.lua

--- .config/nvim/lua/yyossy/plugins/toggleterm.lua
+++ .config/nvim/lua/yyossy/plugins/toggleterm.lua
@@ -6,7 +6,7 @@ return {
       direction = "float",
       float_opts = {
         border = "curved", -- Window border style: 'single', 'double', 'shadow', 'curved'
-        winblend = 15, -- Transparency: specify a value between 0 (opaque) and 100 (fully transparent)
+        winblend = 30, -- Transparency: specify a value between 0 (opaque) and 100 (fully transparent)
       },
     })
   end,
