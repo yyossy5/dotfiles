6512608fca448943b642026068e701c694acdfe3
teihenn <hm.hr.yossy@gmail.com>
2025-03-20 16:58:22 +0900

    Add keymaps to open diffview with origin/main,develop and HEAD

--- .config/nvim/lua/yyossy/plugins/diffview.lua
+++ .config/nvim/lua/yyossy/plugins/diffview.lua
@@ -3,8 +3,14 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   event = "VeryLazy",
   keys = {
-    { "<leader>lo", "<cmd>DiffviewOpen<CR>", desc = "Open Diffview with full file view" },
+    { "<leader>lo", "<cmd>DiffviewOpen<CR>", desc = "Open Diffview" },
     { "<leader>lc", "<cmd>DiffviewClose<CR>", desc = "Close Diffview" },
+    {
+      "<leader>ld",
+      "<cmd>DiffviewOpen origin/develop...HEAD<CR>",
+      desc = "Open Diffview with origin/develop and HEAD",
+    },
+    { "<leader>lm", "<cmd>DiffviewOpen origin/main...HEAD<CR>", desc = "Open Diffview with origin/main and HEAD" },
     { "<leader>lh", "<cmd>DiffviewFileHistory<CR>", desc = "Open DiffviewFileHistory" },
   },
   opts = {
