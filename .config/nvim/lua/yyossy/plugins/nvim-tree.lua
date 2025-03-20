39ea13fb378e7c5487c2b04cc4bf43b3ccabe2fa
teihenn <hm.hr.yossy@gmail.com>
2025-03-20 10:55:49 +0900

    Change open horizontal split keymap of nvim-tree

--- .config/nvim/lua/yyossy/plugins/nvim-tree.lua
+++ .config/nvim/lua/yyossy/plugins/nvim-tree.lua
@@ -8,7 +8,23 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    local function my_on_attach(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- Default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- Custom mappings
+      vim.keymap.set("n", "<C-h>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+      vim.keymap.set("n", "<C-x>", "<Nop>", opts("Disable <C-x>"))
+    end
+
     nvimtree.setup({
+      on_attach = my_on_attach,
       view = {
         width = 35,
         relativenumber = true,
@@ -52,5 +68,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
