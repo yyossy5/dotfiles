d8964e2716136c93a3707a65a893b93261c6c5a2
teihenn <hm.hr.yossy@gmail.com>
2025-03-20 13:48:41 +0900

    Add buffer_manager keymaps to reorder buffers

--- .config/nvim/lua/yyossy/plugins/buffer_manager.lua
+++ .config/nvim/lua/yyossy/plugins/buffer_manager.lua
@@ -1,21 +1,28 @@
 return {
-  {
-    "j-morano/buffer_manager.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local buffer_manager = require("buffer_manager")
-      buffer_manager.setup({
-        select_menu_item_commands = {
-          edit = { key = "<CR>", command = "edit" },
-          split = { key = "<C-h>", command = "split" },
-          vsplit = { key = "<C-v>", command = "vsplit" },
-          delete = { key = "<C-x>", command = "bdelete" },
-        },
-      })
+  "j-morano/buffer_manager.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local buffer_manager = require("buffer_manager")
+    buffer_manager.setup({
+      select_menu_item_commands = {
+        edit = { key = "<CR>", command = "edit" },
+        split = { key = "<C-h>", command = "split" },
+        vsplit = { key = "<C-v>", command = "vsplit" },
+        delete = { key = "<C-x>", command = "bdelete" },
+      },
+    })
 
-      vim.keymap.set("n", "<leader>d", function()
-        require("buffer_manager.ui").toggle_quick_menu()
-      end, { desc = "Open Buffer Manager" })
-    end,
-  },
+    vim.keymap.set("n", "<leader>d", function()
+      require("buffer_manager.ui").toggle_quick_menu()
+    end, { desc = "Open Buffer Manager" })
+
+    -- https://github.com/j-morano/buffer_manager.nvim?tab=readme-ov-file#reorder-buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "buffer_manager",
+      callback = function()
+        vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { buffer = true })
+        vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { buffer = true })
+      end,
+    })
+  end,
 }
