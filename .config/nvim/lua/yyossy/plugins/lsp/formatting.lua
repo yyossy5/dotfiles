bfc5c9fa48432494371d4ac6e4f5474b4c34c82e
teihenn <hm.hr.yossy@gmail.com>
2025-03-26 00:20:33 +0900

    Disable autoformat on certain filetypes

--- .config/nvim/lua/yyossy/plugins/lsp/formatting.lua
+++ .config/nvim/lua/yyossy/plugins/lsp/formatting.lua
@@ -16,20 +16,27 @@ return {
         lua = { "stylua" },
         python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-        -- Disable auto-formatting
-        filter = function(bufnr)
-          -- Ensure bufnr is a valid number
-          if type(bufnr) ~= "number" then
-            return false
-          end
-          local bufname = vim.api.nvim_buf_get_name(bufnr)
-          return not string.match(bufname, "%.java$")
-        end,
-      },
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#autoformat-with-extra-features
+        local ignore_filetypes = {
+          "java",
+        }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        -- Disable autoformat for files in a certain path
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+        -- ...additional logic...
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
