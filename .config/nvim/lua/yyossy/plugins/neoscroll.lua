6d94322c6a808d65195ea9d197ef1859e89cc459
teihenn <hm.hr.yossy@gmail.com>
2025-03-26 00:25:00 +0900

    Speedup neoscroll

--- .config/nvim/lua/yyossy/plugins/neoscroll.lua
+++ .config/nvim/lua/yyossy/plugins/neoscroll.lua
@@ -1,11 +1,39 @@
 -- https://github.com/karb94/neoscroll.nvim
 return {
   "karb94/neoscroll.nvim",
-  config = function(_, opts)
-    require("neoscroll").setup(opts)
+  config = function()
+    local neoscroll = require("neoscroll")
 
-    -- Restore the default behavior of <C-e> and <C-y>
-    vim.keymap.set("n", "<C-e>", "", { noremap = true, silent = true }) -- Scroll down one line
-    vim.keymap.set("n", "<C-y>", "", { noremap = true, silent = true }) -- Scroll up one line
+    neoscroll.setup({
+      easing = "quadratic",
+      hide_cursor = true,
+      stop_eof = true,
+      respect_scrolloff = false,
+      performance_mode = true,
+    })
+
+    local keymap = {
+      ["<C-u>"] = function()
+        neoscroll.ctrl_u({ duration = 150, easing = "sine" })
+      end,
+      ["<C-d>"] = function()
+        neoscroll.ctrl_d({ duration = 150, easing = "sine" })
+      end,
+      ["<C-b>"] = function()
+        neoscroll.ctrl_b({ duration = 300, easing = "circular" })
+      end,
+      ["<C-f>"] = function()
+        neoscroll.ctrl_f({ duration = 300, easing = "circular" })
+      end,
+    }
+
+    local modes = { "n", "v", "x" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func, { silent = true })
+    end
+
+    -- C-e / C-y default behavior(Scrolling 1 line)
+    vim.keymap.set("n", "<C-e>", "<C-e>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<C-y>", "<C-y>", { noremap = true, silent = true })
   end,
 }
