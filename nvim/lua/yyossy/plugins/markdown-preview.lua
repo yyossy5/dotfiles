90cb540c6b0b33ca43cd765f5508a31956bdf242
teihenn <hm.hr.yossy@gmail.com>
2025-03-27 01:34:53 +0900

    Disable auto close of markdown-preview

--- nvim/lua/yyossy/plugins/markdown-preview.lua
+++ nvim/lua/yyossy/plugins/markdown-preview.lua
@@ -7,6 +7,7 @@ return {
   build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_auto_close = 0 -- Keep browser preiview tab open when closing markdown buffer
   end,
   keys = {
     { "<leader>mk", "<cmd>MarkdownPreviewToggle<CR>", ft = "markdown", desc = "Markdown Preview Toggle" },
