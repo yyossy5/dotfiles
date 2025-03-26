a00a9a27db805d7e827a27dcb9b2b9dcfc76ead2
teihenn <hm.hr.yossy@gmail.com>
2025-03-27 01:38:54 +0900

    Enable a keymap to show buffer diagnostics

--- nvim/lua/yyossy/plugins/lsp/lspconfig.lua
+++ nvim/lua/yyossy/plugins/lsp/lspconfig.lua
@@ -47,8 +47,8 @@ return {
         opts.desc = "Smart rename"
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-        -- opts.desc = "Show buffer diagnostics"
-        -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+        opts.desc = "Show buffer diagnostics"
+        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
         -- opts.desc = "Show line diagnostics"
         -- keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
