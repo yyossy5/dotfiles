d475687dea18a2ed166e36fb4d39d2737c6c4289
teihenn <hm.hr.yossy@gmail.com>
2025-03-28 23:33:22 +0900

    Review gitsigns settings

--- nvim/lua/yyossy/plugins/git/gitsigns.lua
+++ nvim/lua/yyossy/plugins/git/gitsigns.lua
@@ -17,19 +17,19 @@ return {
       map("n", "[h", gs.prev_hunk, "Prev Hunk")
 
       -- Actions
-      -- map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-      -- map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-      -- map("v", "<leader>hs", function()
-      --   gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      -- end, "Stage hunk")
-      -- map("v", "<leader>hr", function()
-      --  gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      -- end, "Reset hunk")
+      map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+      map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+      map("v", "<leader>hs", function()
+        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "Stage hunk")
+      map("v", "<leader>hr", function()
+        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "Reset hunk")
 
-      -- map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
-      -- map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+      map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+      map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
 
-      -- map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
+      map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
 
       map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
 
@@ -38,10 +38,15 @@ return {
       end, "Blame line")
       --map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
 
+      map("n", "<leader>hQ", function()
+        gs.setqflist("all")
+      end)
+      map("n", "<leader>hq", gs.setqflist)
+
       map("n", "<leader>hd", gs.diffthis, "Diff this")
-      map("n", "<leader>hD", function()
-        gs.diffthis("~")
-      end, "Diff this ~")
+      -- map("n", "<leader>hd", function()
+      --   gs.diffthis("~")
+      -- end, "Diff this ~")
 
       -- Text object
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
