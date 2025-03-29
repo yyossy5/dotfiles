66166e6a4de382a449736908efc06dde176a31a7
teihenn <hm.hr.yossy@gmail.com>
2025-03-29 18:59:13 +0900

    Fix commit revision of dropbar
    Probably latest ver contains bug

--- nvim/lua/yyossy/plugins/dropbar.lua
+++ nvim/lua/yyossy/plugins/dropbar.lua
@@ -1,5 +1,6 @@
 return {
   "Bekaboo/dropbar.nvim",
+  commit = "873ba43f83398fd0e28880cf98fd89e6ce667c51",
   -- optional, but required for fuzzy finder support
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
