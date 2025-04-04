e6f28a37000c01e4f3beafcaf24fb265ef0b3269
teihenn <hm.hr.yossy@gmail.com>
2025-04-04 20:24:01 +0900

    Change width of buffer manager

--- nvim/lua/yyossy/plugins/buffer_manager.lua
+++ nvim/lua/yyossy/plugins/buffer_manager.lua
@@ -12,7 +12,7 @@ return {
       },
       line_keys = "1234567890",
       focus_alternate_buffer = false,
-      width = 100,
+      width = 110,
       height = 30,
       short_file_names = false,
       show_depth = true,
