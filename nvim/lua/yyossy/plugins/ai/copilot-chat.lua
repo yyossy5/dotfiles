30808e2361475c7c915da35ada147d673931cc20
yyossy5 <hm.hr.yossy@gmail.com>
2025-05-24 10:53:14 +0900

    Disable copilot.nvim and CopilotChat.nvim

--- nvim/lua/yyossy/plugins/ai/copilot-chat.lua
+++ nvim/lua/yyossy/plugins/ai/copilot-chat.lua
@@ -2,6 +2,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    enabled = false,
     dependencies = {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
--- nvim/lua/yyossy/plugins/ai/copilot.lua
+++ nvim/lua/yyossy/plugins/ai/copilot.lua
@@ -1,5 +1,6 @@
 -- https://docs.github.com/ja/copilot/using-github-copilot/getting-code-suggestions-in-your-ide-with-github-copilot?tool=vimneovim
 return {
   "github/copilot.vim",
+  enabled = false,
   lazy = false,
 }
