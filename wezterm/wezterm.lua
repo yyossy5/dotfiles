c362210506b4ac6f60b0d275f6abe1f5f5ccfd43
yyossy5 <hm.hr.yossy@gmail.com>
2025-06-06 02:54:09 +0900

    Update wezterm.lua for appearance

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -113,4 +113,48 @@ return {
 	key_tables = {
 		copy_mode = wezterm.gui.default_key_tables().copy_mode,
 	},
+
+	-- ==================== Appearance ====================
+	-- https://wezterm.org/config/appearance.html
+
+	-- カラースキーム設定
+	color_scheme = "Oxocarbon Dark (Gogh)",
+
+	-- フォント設定
+	font = wezterm.font("Hack Nerd Font Mono"),
+	font_size = 12.5,
+
+	-- カスタム色設定（ANSI色を上書き）
+	colors = {
+		-- ANSIカラーのカスタマイズ
+		ansi = {
+			"#161616", -- black
+			"#3ddbd9", -- red
+			"#33b1ff", -- green
+			"#ee5396", -- yellow
+			"#42be65", -- blue
+			"#be95ff", -- magenta
+			"#ff7eb6", -- cyan
+			"#f2f4f8", -- white
+		},
+		brights = {
+			"#585858", -- bright black
+			"#3ddbd9", -- bright red
+			"#33b1ff", -- bright green
+			"#ee5396", -- bright yellow
+			"#42be65", -- bright blue
+			"#be95ff", -- bright magenta
+			"#ff7eb6", -- bright cyan
+			"#f2f4f8", -- bright white
+		},
+
+		-- 背景色設定
+		background = "#0e1018", -- ダークなネイビーブルー
+
+		-- その他の色設定
+		cursor_bg = "#f2f4f8", -- カーソルの背景色
+		cursor_fg = "#161616", -- カーソルの前景色
+		selection_bg = "#353a45", -- 選択範囲の背景色
+		selection_fg = "#f2f4f8", -- 選択範囲の前景色
+	},
 }
