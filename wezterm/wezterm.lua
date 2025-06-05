efeb04926cae8593c2289e14f039a429d784f088
yyossy5 <hm.hr.yossy@gmail.com>
2025-06-06 02:15:20 +0900

    WezTerm: ウインドウ移動系のキーバインドを追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,6 +18,23 @@ return {
 		-- 新しいウインドウを作成（tmuxのPrefix + c相当）
 		{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 
+		-- 前のウインドウに移動（tmuxのPrefix + p相当）
+		{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+
+		-- 次のウインドウに移動（tmuxのPrefix + n相当）
+		{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+
+		-- 数字キーで指定したウインドウに移動（tmuxのPrefix + 数字相当）
+		{ key = "1", mods = "LEADER", action = act.ActivateTab(0) },
+		{ key = "2", mods = "LEADER", action = act.ActivateTab(1) },
+		{ key = "3", mods = "LEADER", action = act.ActivateTab(2) },
+		{ key = "4", mods = "LEADER", action = act.ActivateTab(3) },
+		{ key = "5", mods = "LEADER", action = act.ActivateTab(4) },
+		{ key = "6", mods = "LEADER", action = act.ActivateTab(5) },
+		{ key = "7", mods = "LEADER", action = act.ActivateTab(6) },
+		{ key = "8", mods = "LEADER", action = act.ActivateTab(7) },
+		{ key = "9", mods = "LEADER", action = act.ActivateTab(8) },
+
 		-- コピーモード
 		{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
 
